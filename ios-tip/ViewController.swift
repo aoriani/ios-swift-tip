0c89deb46b87b5160f2c1992e484dce0a8ef4310
Andre Oriani <aoriani@gmail.com>
2016-01-13 20:51:15 -0800

    Layout, outlets and events

--- ios-tip/ViewController.swift
+++ ios-tip/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSegControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onChange(sender: AnyObject) {
+        print("onChange called")
+    }
 
 }
 
